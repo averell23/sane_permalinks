7dd3c8b2f97b618465188b90d5d69e5690519850
Daniel Hahn <dha@betterplace.org>
2012-06-19 19:59:29 +0200

    Another fix

--- lib/sane_permalinks/sanitizer.rb
+++ lib/sane_permalinks/sanitizer.rb
@@ -5,7 +5,7 @@ module SanePermalinks
 
   def sanitize_param(param)
     return nil unless param
-    param.gsub(/[$…]*/, '').to_url
+    param.gsub(/[$…»«]/, '').to_url # The gsub throws out some stuff we don't trust stringex with... ;-)
   end
 
 end
